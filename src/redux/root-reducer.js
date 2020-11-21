ece75f9e8c435c407a5f9892d848682830b9f5af
xsmolova <smolova@positive.sk@users.noreply.github.com>
2020-11-21 21:36:14 +0100

    local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,23 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+
+//local storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']  //only thing we store is cart
+}
+
 //create root reducer object
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+// modified version of root reducer -> with persistance capabilities
+export default persistReducer(persistConfig, rootReducer);
