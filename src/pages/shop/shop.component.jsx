9cfa6b0114ac7721a77d60a9355f2b8f0bbfcb1e
xsmolova <smolova@positive.sk@users.noreply.github.com>
2020-12-03 15:59:37 +0100

    redux thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import {createStructuredSelector} from 'reselect';
 import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
@@ -7,71 +8,39 @@ import CollectionPage from '../collection/collection.component';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-import { firestore, convertCollectionsSnpshotToMap } from '../../firebase/firebase.utils';
-import { updateCollections } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart, fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching} from '../../redux/shop/shop.selectors'
+
 
 const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends Component {
-    state = {
-        loading: true
-    };
-
-    unsubscribeFromSnapshot = null;
 
     componentDidMount() {
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
-
-        // whenever the collectionRef updates or code gets run for the first time,
-        // this collectionRef send us the snapshot representing the code of our
-        // collection objects array at the time when this code renders
-       
-        /*  this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnpshotToMap(snapshot);
-            updateCollections(collectionsMap)
-            this.setState({loading:false});
-        }); */
-
-        // we can use this but page wouldnt be refreshed with new data
-        // since we are not subscribing on event onSnapshot. It will be filled with data only on componentDidMount 
-    
-        collectionRef.get().then(snapshot => {
-            const collectionsMap = convertCollectionsSnpshotToMap(snapshot);
-            updateCollections(collectionsMap)
-            this.setState({loading:false});
-        });
-
-        // Making rest calls - firestore https://firebase.google.com/docs/firestore/use-rest-api
-        
-        /* fetch('https://firestore.googleapis.com/v1/projects/crown-db-275ad/databases/(default)/documents/collections')
-        .then(response => response.json())
-        .then(collections => console.log(collections)); */
+        const {fetchCollectionsStartAsync} = this.props;
+        fetchCollectionsStartAsync();
     }
-
-    componentWillUnmount() {
-
-    }
-
 
     render() {
-        const { match } = this.props;
-        const {loading} = this.state;
+        const { match, isCollectionFetching } = this.props;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props}/>}></Route>
-                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props}/>}></Route>
+                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props}/>}></Route>
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props}/>}></Route>
             </div>
         );
     }
 
 }
 
+const mapStateToProps = createStructuredSelector({
+    isCollectionFetching: selectIsCollectionFetching
+});
 
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 })
 
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import {createStore, appyMiddleware, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 // Logger only in development, not in production
 if(process.env.NODE_ENV === 'development'){
