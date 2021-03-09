d6a8a145fd40b26372b3f445390047d19576bf17
xsmolova <smolova@positive.sk@users.noreply.github.com>
2021-03-09 09:40:59 +0100

    mobile friendly

--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -8,6 +8,12 @@ export const HeaderContainer = styled.div`
     position: relative;
     justify-content: space-between;
     margin-bottom: 25px;
+
+    @media screen and (max-width: 800px){
+        height:60px;
+        padding:10px;
+        margin-bottom:20px;
+    }
 `;
 
 export const LogoContainer = styled(Link)`
@@ -15,6 +21,11 @@ export const LogoContainer = styled(Link)`
     width: 70px;
     display: flex;
     align-items: center;
+
+    @media screen and (max-width: 800px){
+        width:50px;
+        padding:0;
+    }
 `;
 
 export const OptionsContainer = styled.div`
@@ -23,6 +34,10 @@ export const OptionsContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+
+    @media screen and (max-width: 800px){
+        width: 80%;
+    }
 `;
 
 
